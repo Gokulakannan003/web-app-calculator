6e32b981c595b4e2e343c6e36e95b07e77312711
𝓖𝓸𝓴𝓾𝓵𝓪𝓴𝓪𝓷𝓷𝓪𝓷 <gokulakannan2008cs@gmail.com>
2024-09-08 15:45:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function displayvalue(val) {
+    document.getElementById("display").value += val;
+}
+
+ function clearo(){
+    document.getElementById("display").value ="";
+
+ }
+
+ function equal(){
+    var a = document.getElementById("display").value;
+    try{
+        result = eval(a)
+        document.getElementById("display").value = result;
+    }
+    catch{
+        alert("Hi bro. You give the invalid syntax ")
+        clearo()
+    }
+ }
